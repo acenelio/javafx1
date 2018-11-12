f798bff64862ba4e1eed79897f7da3c9941178c5
Nelio Alves <acenelio@gmail.com>
2018-11-12 19:31:41 -0200

    Testing FXML file

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,25 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+	    		Scene scene = new Scene(parent);
+	        	stage.setScene(scene);
+	        	stage.show();
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
